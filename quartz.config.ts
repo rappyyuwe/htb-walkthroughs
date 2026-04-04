17cb207ddac3c04f3a7377b63402c18dcbe62fd4
rappyyuwe <esmekhoo90@gmail.com>
2026-04-05 00:18:09 +0800

    Quartz sync: Apr 5, 2026, 12:18 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "rappyyuwe.github.io/htb-walkthroughs",
+    baseUrl: "rappyyuwe.github.io/htb-walkthroughs/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/rappyyuwe",
+    
     },
   }),
 }
@@ -38,7 +38,10 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "Boxes",
+      folderClickBehavior: "collapse"
+    }),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +65,10 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({      
+      title: "Boxes",
+      folderClickBehavior: "collapse"  
+    }),
   ],
   right: [],
 }
